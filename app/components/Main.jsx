b009ad8b9a620624b1111b490205c35ba16ca13a
susuaung <susuaung.ms@gmail.com>
2017-04-06 16:15:06 +0900

    Add navigation

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,11 +1,11 @@
 var React = require('react');
-
+var Navigation = require('Navigation');
 var Main = (props) => {
     return (
       <div>
+        <Navigation />
         <div>
           <div>
-            <p>Main.jsx Rendered</p>
             {props.children}
           </div>
         </div>
